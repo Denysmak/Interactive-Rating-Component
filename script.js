b6d54938af41be10e42ad5c5c645f808efb2b43a
Denysmak <denysmakene13@gmail.com>
2022-07-25 13:02:07 -0300

    when page loads 5 stars is clicked

--- script.js
+++ script.js
@@ -12,93 +12,49 @@ let estrelas;
 
 // This makes que main div change when we click the button
 
-
-
-
+window.onload = () => {
+   five.click()
+   
+}
 
 // Makes the number/button get orange when we click on it, and makes the other numbers to have the same background
 one.addEventListener("click", function(){
-this.style.backgroundColor = "orange"
-this.style.color = "white"
-two.style.backgroundColor = "hsl(213, 20%, 22%)"
-three.style.backgroundColor = "hsl(213, 20%, 22%)"
-four.style.backgroundColor = "hsl(213, 20%, 22%)"
-five.style.backgroundColor = "hsl(213, 20%, 22%)"
-
 estrelas = 1;
-
-
-
-
+   five.style.backgroundColor = "hsl(213, 20%, 22%)"
 })
 
 
 two.addEventListener("click", function () {
-    this.style.backgroundColor = "orange"
-    this.style.color = "white"
-    one.style.backgroundColor = "hsl(213, 20%, 22%)"
-    three.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-    four.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-    five.style.backgroundColor = "hsl(213, 20%, 22%)"
- 
-   estrelas = 2;
-
+estrelas = 2;
 })
 three.addEventListener("click", function () {
-    this.style.backgroundColor = "orange"
-    this.style.color = "white"
-    one.style.backgroundColor = "hsl(213, 20%, 22%)"
-    two.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-    four.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-    five.style.backgroundColor = "hsl(213, 20%, 22%)"
-
  estrelas = 3;
-
 })
 
 four.addEventListener("click", function () {
-    this.style.backgroundColor = "orange"
-    this.style.color = "white"
-    one.style.backgroundColor = "hsl(213, 20%, 22%)"
-    three.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-    two.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-    five.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-   estrelas = 4;
-
+ estrelas = 4;
 })
 
 five.addEventListener("click", function () {
-    this.style.backgroundColor = "orange"
-    this.style.color = "white"
-    one.style.backgroundColor = "hsl(213, 20%, 22%)"
-    three.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-    four.style.backgroundColor = "hsl(213, 20%, 22%)"
-
-    two.style.backgroundColor = "hsl(213, 20%, 22%)"
-
    estrelas = 5;
-  
-
 })
 
-// ======================
-
 submit.addEventListener("click", function () {
+  
+    if(estrelas > 0 ){
 
     first.style.display = "none"
     second.style.display = "inline-block"
-    // estrelas.style.color = "rgb(255, 123, 0)"
+   
   
     
     
 
-    selected.innerHTML = "You selected " + estrelas + " out of 5"
+    selected.innerHTML = "You selected " + estrelas + " out of 5"}
+     else {
+        first.style.display = "none"
+        second.style.display = "inline-block"
+  selected.innerText = "You haven't selected a number"
+
+    }
 })
